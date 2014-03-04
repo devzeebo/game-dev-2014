eb8d8041475be213ee9ebd6cab4397ce0fc96465
Eric Siebeneich <eric.siebeneich@gmail.com>
2014-03-04 17:06:51 -0600

    Added LookAt2D to Utilities

--- Assets/Scripts/Input/TouchController.cs
+++ Assets/Scripts/Input/TouchController.cs
@@ -1,5 +1,6 @@
 ﻿using UnityEngine;
 using System.Collections;
+using System.Collections.Generic;
 
 [RequireComponent(typeof(InputHandler),typeof(Grid))]
 public class TouchController : MonoBehaviour {
@@ -7,19 +8,29 @@ public class TouchController : MonoBehaviour {
 	private InputHandler handler;
 	private Grid grid;
 	public GameObject temp;
+
+	private List<GameObject> objs;
+
 	// Use this for initialization
 	void Start () {
 		grid = GetComponent<Grid>();
 		handler = GetComponent<InputHandler>();
+
+		objs = new List<GameObject>();
 	}
 	
 	// Update is called once per frame
 	void Update () {
 		handler.handleInput();
+
+		foreach (GameObject obj in objs) {
+			obj.LookAt2D(grid.ScreenToWorldPosition(Input.mousePosition));
+		}
+
 		foreach(InputEvent e in handler.Events){
 			if(e.phase == TouchPhase.Began){
 				Debug.Log(temp);
-				temp.GetComponent<CustomTower>().Spawn(grid.ScreenToWorldPosition(e.position));
+				objs.Add(temp.GetComponent<CustomTower>().Spawn(grid.ScreenToWorldPosition(e.position)));
 			}
 		}
 	}
