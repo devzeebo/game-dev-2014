33e4c9b1872a10b0a99c02372430ad93e2f46fae
Eric Siebeneich <eric.siebeneich@gmail.com>
2014-03-06 16:41:03 -0600

    Added back button

---
+++ Assets/Scripts/GUI/BackButton.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class BackButton : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+	void OnGUI() {
+		if (GUI.Button(new Rect(this.Width(0.1f), this.Height(0), this.Width(0.1f), this.Height(0.05f)), "Back")) {
+			Application.LoadLevel(1);
+		}
+	}
+}
