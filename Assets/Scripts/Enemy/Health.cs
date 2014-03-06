45fd5a09de33202092ff5eddc359a4cb29535128
James Garcia <cooljj_20@sbcglobal.net>
2014-03-06 14:19:57 -0600

    Health Attempts

---
+++ Assets/Scripts/Enemy/Health.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Health : MonoBehaviour {
+
+	public int health = 100; 
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+	}
+
+	int Damage(int damage) {
+		health -= damage;
+		return health;
+	}
+}
