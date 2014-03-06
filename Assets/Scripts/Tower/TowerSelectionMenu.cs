586172a0fff8209034ea9c11df68ee9f1800e2e4
Eric Siebeneich <eric.siebeneich@gmail.com>
2014-03-06 12:32:31 -0600

    Added static tower selection object

---
+++ Assets/Scripts/Tower/TowerSelectionMenu.cs
@@ -0,0 +1,35 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+public class TowerSelection : MonoBehaviour {
+
+	List<CustomTower> towers;
+
+	// Use this for initialization
+	void Start () {
+		DontDestroyOnLoad(gameObject);
+		instance = this;
+
+		towers = new List<CustomTower>(8);
+	}
+
+	private static TowerSelection instance;
+	public static TowerSelection Instance {
+		get {
+			return instance;
+		}
+		private set {
+			instance = value;
+		}
+	}
+
+	public CustomTower this[int x] {
+		get {
+			return towers[x];
+		}
+		set {
+			towers[x] = value;
+		}
+	}
+}
