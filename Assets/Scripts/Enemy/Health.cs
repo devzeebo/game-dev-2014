63016d66457326582926da26a6383ae60176ce85
James Garcia <cooljj_20@sbcglobal.net>
2014-03-18 17:55:57 -0500

    Added Pikachu

--- Assets/Scripts/Enemy/Health.cs
+++ Assets/Scripts/Enemy/Health.cs
@@ -3,12 +3,14 @@ using System.Collections;
 
 public class Health : MonoBehaviour {
 
-	public float health = 100;
-
+	public float maxHealth = 100;
+	[HideInInspector]
+	public float health;
 	public GameObject HealthBar;
 
 	// Use this for initialization
 	void Start () {
+		health = maxHealth;
 		HealthBar = (GameObject)GameObject.Instantiate(HealthBar, transform.position, Quaternion.identity);
 		HealthBar.transform.parent = transform;
 		HealthBar.transform.position -= new Vector3(0, -0.7f, 0);
@@ -16,7 +18,7 @@ public class Health : MonoBehaviour {
 	
 	// Update is called once per frame
 	void Update () {
-		HealthBar.transform.localScale = new Vector3(1f, health / 100);
+		HealthBar.transform.localScale = new Vector3(health/ maxHealth, 1f);
 	}
 
 	public float Damage(float damage) {
