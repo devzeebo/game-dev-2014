f3a176a7494a660e19fa72d8d32eed2fbc8920f4
James Garcia <cooljj_20@sbcglobal.net>
2014-03-25 17:43:49 -0500

    Added Sticky Script

---
+++ Assets/Scripts/Projectile/Sticky.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Sticky : Projectile {
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+	}
+
+	public override void HitEnemy(GameObject enemy)
+	{
+		enemy.GetComponent<speed> = enemy.GetComponent<speed> * .5;
+	}
+}
