c0606ecfe6adf6b6c52619e9d23714f1f59147b8
Eric Siebeneich <eric.siebeneich@gmail.com>
2014-04-01 16:50:41 -0500

    Fixed some stuff

---
+++ Assets/Scripts/Player/PlayerStats.cs
@@ -0,0 +1,29 @@
+﻿using UnityEngine;
+using System.Collections.Generic;
+
+public class PlayerStats : MonoBehaviour {
+
+	public int TotalJunk;
+	
+	public Dictionary<string, float> TowerSpentExperience;
+	public Dictionary<string, float> TowerTotalExperience;
+
+	// Use this for initialization
+	void Start () {
+		TowerSpentExperience = new Dictionary<string, float>();
+		TowerTotalExperience = new Dictionary<string, float>();
+	}
+
+	private static PlayerStats instance;
+	public static PlayerStats Instance {
+		get {
+			return instance;
+		}
+		private set {
+			instance = value;
+		}
+	}
+
+	void AddExperience(string component, float amount) {
+	}
+}
