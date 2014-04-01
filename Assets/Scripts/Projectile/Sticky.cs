eec9e061994ea3ff0f55fe918875532ce64e9fe5
James Garcia <cooljj_20@sbcglobal.net>
2014-04-01 17:56:28 -0500

    Sticky Works

--- Assets/Scripts/Projectile/Sticky.cs
+++ Assets/Scripts/Projectile/Sticky.cs
@@ -3,18 +3,19 @@ using System.Collections;
 
 public class Sticky : Projectile {
 
-	// Use this for initialization
-	void Start () {
-	
-	}
-	
-	// Update is called once per frame
-	void Update () {
+	public float initialSpeed;
 
-	}
+	public float effectTime;
+	public float speedModifier;
 
-	public override void HitEnemy(GameObject enemy)
-	{
+	public override void HitEnemy(GameObject enemy) {
 
+		StickyEffect effect;
+		if ((effect = enemy.GetComponent<StickyEffect>()) == null) {
+			effect = enemy.AddComponent<StickyEffect>();
+		}
+
+		effect.speedModifier = speedModifier;
+		effect.timeRemaining = effectTime;
 	}
 }
