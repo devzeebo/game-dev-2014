01ba4c063a171e17ddd8563e0f8f3402f174e6c0
Eric Siebeneich <eric.siebeneich@gmail.com>
2014-04-18 17:54:05 -0500

    Added proper Scene AutoLoad

---
+++ Assets/Scripts/Player/GameStats.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class GameStats : MonoBehaviour {
+
+	public float CashPerSecond = 5f;
+
+	public float Cash = 100;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		Cash += CashPerSecond * Time.deltaTime;
+	}
+
+	void OnGUI() {
+		GUI.Label(this.CenteredRect(this.Width(.8f), this.Height(.1f), this.Width(.1f), this.Height(.1f)), "" + ((int)Cash));
+	}
+}
