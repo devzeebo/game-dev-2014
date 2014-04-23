0500144e598c7391b36c194790a41cfe39bcdc7a
Eric Siebeneich <eric.siebeneich@gmail.com>
2014-04-23 17:07:34 -0500

    Added bounty to creeps

---
+++ Assets/Scripts/Player/Bounty.cs
@@ -0,0 +1,11 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Bounty : MonoBehaviour {
+
+	public float Amount = 10;
+
+	void OnDestroy() {
+		GameStats.CollectBounty(Amount);
+	}
+}
