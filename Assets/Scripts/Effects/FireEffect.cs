e157b2446bd6c73db5535bb5a1bc713b7c243c79
balduron04 <balduron04@yahoo.com>
2014-04-22 19:19:28 -0500

    Fixed, do not need help anymore.

---
+++ Assets/Scripts/Effects/FireEffect.cs
@@ -0,0 +1,28 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class FireEffect : MonoBehaviour
+{
+
+    public float initialSpeed;
+
+    public float speedModifier;
+    public float timeRemaining;
+
+    // Use this for initialization
+    void Start()
+    {
+        initialSpeed = gameObject.GetComponent<FollowPath>().speed;
+        gameObject.GetComponent<FollowPath>().speed = gameObject.GetComponent<FollowPath>().speed * speedModifier;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if ((timeRemaining -= Time.deltaTime) < 0)
+        {
+            gameObject.GetComponent<FollowPath>().speed = initialSpeed;
+            DestroyImmediate(this);
+        }
+    }
+}
