58eed55eaa9c358772920dc4e89c99a2aca0d8e5
balduron04 <balduron04@yahoo.com>
2014-04-24 15:27:44 -0500

    Moved function to limit errors

--- Assets/Scripts/Enemy/Spacer.cs
+++ Assets/Scripts/Enemy/Spacer.cs
@@ -5,11 +5,11 @@ public class Spacer : MonoBehaviour {
 
 	// Use this for initialization
 	void Start () {
-        Destroy(this);
+
 	}
 	
 	// Update is called once per frame
 	void Update () {
-	
+        Destroy(this);
 	}
 }
