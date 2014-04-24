4c13ef528873da9d791f35bbb3579453c774da78
balduron04 <balduron04@yahoo.com>
2014-04-24 14:51:06 -0500

    Fixing Levels

---
+++ Assets/Scripts/Enemy/Spacer.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Spacer : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+        Destroy(this);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
